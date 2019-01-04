72f0f27f63a6b78c74a0bd0acd1c64ef1126c9bd
Pivrik Artem <artempivrik90@gmail.com>
2019-01-05 00:08:59 +0200

    Updates

---
+++ precache-manifest.ed5f6a8a38d917b011bad84470bde198.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6817dda23da199eaa69",
+    "url": "/test-for-pixelplex/static/css/main.eecc0b2a.chunk.css"
+  },
+  {
+    "revision": "c6817dda23da199eaa69",
+    "url": "/test-for-pixelplex/static/js/main.c6817dda.chunk.js"
+  },
+  {
+    "revision": "2a50b70a295a8050c33b",
+    "url": "/test-for-pixelplex/static/css/1.c5e7f73f.chunk.css"
+  },
+  {
+    "revision": "2a50b70a295a8050c33b",
+    "url": "/test-for-pixelplex/static/js/1.2a50b70a.chunk.js"
+  },
+  {
+    "revision": "0e3f1b5419541444ee1f",
+    "url": "/test-for-pixelplex/static/js/runtime~main.0e3f1b54.js"
+  },
+  {
+    "revision": "f894bce27d96ece589187a465b9dd588",
+    "url": "/test-for-pixelplex/static/media/1.f894bce2.gif"
+  },
+  {
+    "revision": "ae6ebb965362c4a14adf14a2e1d50111",
+    "url": "/test-for-pixelplex/index.html"
+  }
+];
